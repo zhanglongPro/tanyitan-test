6a31cb3187af3b8fb1c33bafe3bd584861d0dd55
zhanglongPro <z59210@126.com>
2020-04-15 20:48:01 +0800

    update

--- src/main/java/top/tanyitan/demo/UserTest.java
+++ src/main/java/top/tanyitan/demo/UserTest.java
@@ -12,6 +12,7 @@ public class UserTest {
     public static void main(String[] args) {
         List<Object> objects = new ArrayList<>();
         objects.add("田岳云");
+        objects.add("八戒");
         System.err.println("admin"+":"+objects);
         System.err.println("admin1");
     }
