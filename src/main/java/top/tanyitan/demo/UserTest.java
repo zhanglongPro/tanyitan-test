505cca9972f2837429d49169dbbe680da4f4d307
zhanglongPro <z59210@126.com>
2020-04-15 20:09:02 +0800

    提交UserTest

---
+++ src/main/java/top/tanyitan/demo/UserTest.java
@@ -0,0 +1,12 @@
+package top.tanyitan.demo;
+
+/**
+ * @description: UserTest
+ * @author: ZhangLong
+ * @time: 2020/4/15 20:07
+ */
+public class UserTest {
+    public static void main(String[] args) {
+        System.err.println("admin");
+    }
+}
